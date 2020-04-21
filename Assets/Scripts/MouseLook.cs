f07bcf9dd6b213f7cbdb0a82f98d4bdad717af71
Broccoli <casper.nesrual@gmail.com>
2020-04-21 14:52:46 +0200

    Updated controller to add object rotation

--- Assets/Scripts/MouseLook.cs
+++ Assets/Scripts/MouseLook.cs
@@ -4,17 +4,29 @@ using UnityEngine;
 
 public class MouseLook : MonoBehaviour
 {
-
+    /////////////////////
+    //PUBLIC VARIABLES//
+    ////////////////////
     public float sensitivity = 100f;
-    public Transform playerBody;
+    public GameObject playerBody;
 
+
+
+    /////////////////////
+    //PRIVATE VARIABLES//
+    ////////////////////
     private float xRotation = 0f;
+    private Transform playerTransform;
+
+
+
 
     // Start is called before the first frame update
     void Start()
     {
         //Lock cursor inside play area
         Cursor.lockState = CursorLockMode.Locked;
+        playerTransform = playerBody.GetComponent<Transform>();
     }
 
     // Update is called once per frame
@@ -23,27 +35,17 @@ public class MouseLook : MonoBehaviour
         float mouseX = Input.GetAxisRaw("Mouse X") * sensitivity * Time.deltaTime;
         float mouseY = Input.GetAxisRaw("Mouse Y") * sensitivity * Time.deltaTime;
 
-        //Negative y so the controls are not inverted
-        xRotation -= mouseY;
-        //Clamp the y value so you can't rotate all the way around and break your neck
-        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
 
-        //Rotate player
-        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
-        playerBody.Rotate(Vector3.up * mouseX);
+        if(playerBody.GetComponent<PlayerController>().canMove)
+        {
+            //Negative y so the controls are not inverted
+            xRotation -= mouseY;
+            //Clamp the y value so you can't rotate all the way around and break your neck
+            xRotation = Mathf.Clamp(xRotation, -90f, 90f);
 
-        //RotateView();
+            //Rotate player
+            transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+            playerTransform.Rotate(Vector3.up * mouseX);
+        }
     }
-
-    //private void RotateView()
-    //{
-    //    //Negative y so the controls are not inverted
-    //    xRotation -= mouseY;
-    //    //Clamp the y value so you can't rotate all the way around and break your neck
-    //    xRotation = Mathf.Clamp(xRotation, -90f, 90f);
-
-    //    //Rotate player
-    //    transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
-    //    playerBody.Rotate(Vector3.up * mouseX);
-    //}
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,21 +4,37 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    /////////////////////
+    //PUBLIC VARIABLES//
+    ////////////////////
     public float speed = 10.0f;
-    public CharacterController controller;
     public float gravity = -9.81f;
+    public bool canMove;
+
+    public CharacterController controller;
+    
     public Camera cam;
 
+
+    /////////////////////
+    //PRIVATE VARIABLES//
+    /////////////////////
     private float pickupLength = 3;
+    private float smooth = 5f;
+
+    private bool carrying;
+
     private Vector3 movement;
     private Vector3 velocity;
     private GameObject carriedObject;
-    private bool carrying;
-    private float smooth = 5f;
+
+
+
 
     // Start is called before the first frame update
     void Start()
     {
+        canMove = true;
         controller = GetComponent<CharacterController>();
     }
 
@@ -27,28 +43,33 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float x = Input.GetAxis("Horizontal");
-        float z = Input.GetAxis("Vertical");
+        //Player movement - limited by variable so we can control held objects
+        if(canMove)
+        {
+            float x = Input.GetAxis("Horizontal");
+            float z = Input.GetAxis("Vertical");
 
-        Vector3 move = transform.right * x + transform.forward * z;
-        controller.Move(move * speed * Time.deltaTime);
+            Vector3 move = transform.right * x + transform.forward * z;
+            controller.Move(move * speed * Time.deltaTime);
+        }
+
     }
 
-    private void FixedUpdate()
+   
+    void FixedUpdate()
     {
-        //Apply gravity because character controllers don't use physics
+        //Fake gravity because character controllers don't use physics
         if(!controller.isGrounded)
         {
-            velocity.y += gravity * Time.deltaTime;
-            controller.Move(velocity*Time.deltaTime);
+            velocity.y += gravity * Mathf.Pow(Time.deltaTime, 2);
+            controller.Move(velocity);
         } else if (controller.isGrounded)
         {
+            //Always apply a small force downwards since sometimes it bugs out and sets isGrounded before actually being on the ground
             velocity.y = -1.0f;
         }
 
 
-
-        
         if (carrying)
         {
             carry(carriedObject);
@@ -56,8 +77,8 @@ public class PlayerController : MonoBehaviour
             {
                 dropObject();
             }
-
-            //Move held object away or closer when scrolling the mousewheel
+            
+            //Use scrollwheel to push or pull the held object
             if (Input.GetAxis("Mouse ScrollWheel") > 0)
             {
                 pickupLength += 0.5f;
@@ -69,20 +90,28 @@ public class PlayerController : MonoBehaviour
                 pickupLength = Mathf.Clamp(pickupLength, 2, 6);
             }
 
+         
+            if (Input.GetMouseButton(1))
+            {
+                rotateObject(carriedObject);
+            }
+            //Since the mouse button input is a boolean, it can be used to determine the canMove variable, so you can't move while rotating objects
+            canMove = !Input.GetMouseButton(1);
+
         }
-        else
+        //Make sure the player can move again after dropping object, and check for pickups again
+        else if(!carrying)
         {
+            canMove = true;
             itemPickup();
         }
     }
 
-
-    #region ItemPickup
     private void itemPickup()
     {
         if(Input.GetMouseButtonDown(0))
         {
-            //Draw a ray to see if we hit a physics object
+            //Raycast and look for a collider with the physicsItem layer (layer 10)
             int layerMask = 1 << 10;
             RaycastHit hit;
             if (Physics.Raycast(cam.transform.position, cam.transform.TransformDirection(Vector3.forward), out hit, pickupLength, layerMask))
@@ -99,9 +128,9 @@ public class PlayerController : MonoBehaviour
         Rigidbody rb = o.GetComponent<Rigidbody>();
         rb.useGravity = false;
         rb.freezeRotation = true;
-        //Lerp the position with delta time to smooth the movement of the object. Also helps with clipping through other objects
+        //Lerp with delta time for smooth movement on the carried object
         o.transform.position = Vector3.Lerp(o.transform.position, cam.transform.position + cam.transform.forward * pickupLength, Time.deltaTime * smooth);
-        //Reset velocity otherwise the carriedObject would freak out
+        //The held object had glitchy behaviour if hit by other objects while being held. To fix this we reset the velocity
         rb.velocity = new Vector3(0,0,0);
     }
 
@@ -114,6 +143,18 @@ public class PlayerController : MonoBehaviour
         pickupLength = 3.0f;
     }
 
-    #endregion
+    private void rotateObject(GameObject o)
+    {
+        canMove = false;
+        Rigidbody rb = o.GetComponent<Rigidbody>();
+        //Unfreeze the object so we can apply torque
+        rb.freezeRotation = false;
+
+        float turnX = Input.GetAxis("Mouse X");
+        float turnY = Input.GetAxis("Mouse Y");
+        float torque = 1000;
+        rb.AddTorque(Vector3.up * torque * -turnX * Time.deltaTime);
+        rb.AddTorque(Vector3.right * torque * turnY * Time.deltaTime);
+    }
 
 }
