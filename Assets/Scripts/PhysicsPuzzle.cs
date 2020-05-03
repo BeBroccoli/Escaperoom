006b275e76193619e1ddf121b0699d2b14fb295b
Broccoli <casper.nesrual@gmail.com>
2020-05-03 21:35:04 +0200

    changed the use object script so now it's more versatile

--- Assets/Scripts/PhysicsPuzzle.cs
+++ Assets/Scripts/PhysicsPuzzle.cs
@@ -5,24 +5,20 @@ using UnityEngine;
 
 public class PhysicsPuzzle : MonoBehaviour
 {
-
-    public Material redMat;
-    public Material greenMat;
     public GameObject player;
     public List<GameObject> questItemList;
     public int questProgress;
     public string puzzleName;
     public UnityEvent puzzleComplete;
 
-    private Renderer r;
     private PlayerController pc;
+
+
+
 
     // Start is called before the first frame update
     void OnEnable()
     {
-        //For testing purposes
-        r = GetComponent<Renderer>();
-
         pc = player.GetComponent<PlayerController>();
 
         //Always have the amount of required objects be the starting progress of the puzzle
@@ -45,6 +41,9 @@ public class PhysicsPuzzle : MonoBehaviour
         questCheck();
     }
 
+
+
+
     //Check to see if a/any puzzle items are inside the "goal"
     private void OnTriggerStay(Collider other)
     {
@@ -65,24 +64,12 @@ public class PhysicsPuzzle : MonoBehaviour
         }
     }
 
-    //private void OnTriggerExit(Collider other)
-    //{
-    //    if (other.CompareTag("puzzleItem"))
-    //    {
-    //        //Debug material change
-    //        r.material = redMat;
-    //    }
-    //}
-
     //Used to check if the puzzle is completed
     private void questCheck()
     {
         if(questProgress <= 0)
         {
             puzzleComplete.Invoke();
-
-            //Debug material change
-            r.material = greenMat;
         }
     }
 }
---
+++ Assets/Scripts/UseObject.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Events;
+using UnityEngine;
+
+public class UseObject : MonoBehaviour
+{
+    [SerializeField]
+    GameObject Item;
+
+    public string animationBool;
+    public UnityEvent onUse;
+
+    private Animator Anim;
+
+    // OnEnable is called when the object is enabled, or at start if it starts as enabled
+    void OnEnable()
+    {
+        if(Item != null)
+        {
+            Anim = Item.GetComponent<Animator>();
+        }
+    }
+
+    //Call this method when hit by ray in player script
+    void HitByRay()
+    {
+        onUse.Invoke();
+    }
+
+    //Sets animation bool of Item. Called when used in the onUse unity event
+    //Creating a method for this since you can't set animation bool through pure unity event
+    public void setAnimBool()
+    {
+        if (!Anim.GetBool(animationBool))
+        {
+            Anim.SetBool(animationBool, true);
+        }
+
+        else if (Anim.GetBool(animationBool))
+        {
+            Anim.SetBool(animationBool, false);
+        }
+    }
+
+
+}
