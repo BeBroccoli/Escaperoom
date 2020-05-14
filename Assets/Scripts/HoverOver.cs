df69d77de5f5432dcdd007997d8722564be765e1
McDingleThais <49029089+McDingleThais@users.noreply.github.com>
2020-05-14 17:54:22 +0200

    Added a cursor

---
+++ Assets/Scripts/HoverOver.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HoverOver : MonoBehaviour
+{
+    [SerializeField]
+    private Image customImage;
+
+    void HitByRay()
+    {
+        customImage.enabled = true;
+    }
+}
