e7409641a621126c06b9168624604282190e7f4f
McDingleThais <49029089+McDingleThais@users.noreply.github.com>
2020-05-19 13:31:39 +0200

    Added Door opening sound effect

---
+++ Assets/Scripts/ADoorOpen.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ADoorOpen : MonoBehaviour
+{
+    AudioSource Audio;
+
+    void Start()
+    {
+     
+    }
+
+    private void OnEnable()
+    {
+        Audio = GetComponent<AudioSource>();
+        Audio.Play(0);
+    }
+
+
+}
