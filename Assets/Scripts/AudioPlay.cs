8cc630ff5446aac2f5367bb7b56b0e7447beb9db
McDingleThais <49029089+McDingleThais@users.noreply.github.com>
2020-05-19 12:59:14 +0200

    Added sound effects to bookshelf and chest

---
+++ Assets/Scripts/AudioPlay.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioPlay : MonoBehaviour
+{
+    AudioSource SFX;
+
+    void Start()
+    {
+        SFX = GetComponent<AudioSource>();
+        SFX.Play(0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
