77fcb3d9245879e5b867b37652c6b560ba64bac4
McDingleThais <49029089+McDingleThais@users.noreply.github.com>
2020-06-02 15:24:13 +0200

    Completed the game

---
+++ Assets/Scripts/BackToMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BackToMenu : MonoBehaviour
+{
+
+    float timeLeft = 60.0f;
+
+    void Update()
+    {
+        timeLeft -= Time.deltaTime;
+        if (timeLeft < 0)
+        {
+            SceneManager.LoadScene("Main Menu");
+        }
+    }
+}
